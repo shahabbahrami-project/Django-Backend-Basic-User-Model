bbeed204922403319c03a350f27a7c7dbe28dd66
Shahab <bahramis@ece.ubc.ca>
2021-04-12 16:20:58 -0700

    first commit

---
+++ app/user/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from user import views
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('createuser/', views.CreateUserView.as_view(), name='createuser'),
+    path('createsuperuser/', views.CreateSuperUserView.as_view(), name='createsuperuser'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('meuser/', views.ManageUserView.as_view(), name='me'),
+    path('mesuperuser/', views.ManageSuperUserView.as_view(), name='me'),
+]
---
+++ app/user/views.py
@@ -0,0 +1,41 @@
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, SuperUserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user"""
+    serializer_class = UserSerializer
+
+class CreateSuperUserView(generics.CreateAPIView):
+    """Create a new superuser"""
+    serializer_class = SuperUserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
+
+
+class ManageSuperUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = SuperUserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
